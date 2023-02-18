def63164b20b74ed420eb8cbac8fc4db060e166a
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-02-18 13:46:55 +0100

    Run rust formatter on project

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -3,8 +3,8 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::fs;
 
 fn character_frequency_benchmark(c: &mut Criterion) {
-	let filename = "benches/bench_text.txt";
-    let text = fs::read_to_string(filename).expect(&format!("File not found: {}",filename));
+    let filename = "benches/bench_text.txt";
+    let text = fs::read_to_string(filename).expect(&format!("File not found: {}", filename));
     c.bench_function("sequential", |b| {
         b.iter(|| sequential_character_frequencies(black_box(&text)))
     });
